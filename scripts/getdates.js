0807cb4bc6ecde0d3fd363fb5bc5e46079b2c174
DanTheMan1830 <dan@lyon.org>
2026-03-14 16:51:17 -0600

    week 1-2

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,7 @@
+const currentyear = document.querySelector("#currentyear");
+
 const today = Date.now();
 
 console.log(today.getFullYear());
+
+currentyear.innerHTML = 'getDay(): <span id="currentyear">${today.getDay()}</span>';
