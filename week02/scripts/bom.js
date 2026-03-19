83175511ec8f7ba5988784538a426001d74a79e5
DanTheMan1830 <dan@lyon.org>
2026-03-19 17:40:11 -0600

    week2

---
+++ week02/scripts/bom.js
@@ -0,0 +1,29 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+
+
+button.addEventListener('button', function () {
+
+    if (input.value.trim() !== '') {
+
+        const li = document.createElement('li');
+        li.textContent = input.value;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        li.appendChild(deleteButton);
+
+        list.appendChild(li);
+
+        input.value = "";
+    }
+
+    input.focus();
+});
