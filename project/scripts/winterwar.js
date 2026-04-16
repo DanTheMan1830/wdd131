8c58e350aa4fb7dd410e07b134a9575c78341f1a
DanTheMan1830 <dan@lyon.org>
2026-04-16 17:32:24 -0600

    project final

--- project/scripts/winterwar.js
+++ project/scripts/winterwar.js
@@ -19,6 +19,42 @@ hamButton.addEventListener("click", () => {
     hamButton.classList.toggle("open");
 });
 
+//////////////
+// War Facts
+//////////////
+const warFacts = [
+    {
+        label: "Date",
+        value: "Nov 1939 – March 1940"
+    },
+    {
+        label: "Invading Force",
+        value: "Soviet Union"
+    },
+    {
+        label: "Defending Force",
+        value: "Finland"
+    },
+    {
+        label: "Location",
+        value: "Karelian Isthmus"
+    }
+];
+
+const labelList = document.querySelector('.label');
+const valueList = document.querySelector('.value');
+
+
+warFacts.forEach(entry => {
+    let liLabel = document.createElement('li');
+    let liValue = document.createElement('li');
+
+    liLabel.textContent = entry.label;
+    liValue.textContent = entry.value;
+
+    labelList.appendChild(liLabel);
+    valueList.appendChild(liValue);
+});
 
 //////////////////////
 // Local Storage Stuff
