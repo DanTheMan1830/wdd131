bb69ccc0ea41a861779483d2582356181e66047c
DanTheMan1830 <dan@lyon.org>
2026-04-16 13:17:38 -0600

    final project update

--- project/scripts/winterwar.js
+++ project/scripts/winterwar.js
@@ -6,6 +6,21 @@ const currentYear = new Date().getFullYear();
 copyrightYear.textContent = currentYear;
 const modifiedDate = document.getElementById("lastModified").innerHTML = document.lastModified;
 
-/////////////////////
-//
-/////////////////////
+/////////////////
+// Hamburger Menu
+/////////////////
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+});
+
+
+//////////////////////
+// Local Storage Stuff
+//////////////////////
+
